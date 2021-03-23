749ac954cde84f6b874320e22272d584b61e4c16
12Me21 <12Me21.mc@gmail.com>
2021-03-23 02:45:42 -0400

    fix inserting etc.

--- gap-buffer.c
+++ gap-buffer.c
@@ -39,27 +39,31 @@ void String_rtrans(const String dest, Index d, const String src, Index s, Index 
 	memmove(&src.props[s], &dest.props[d], length);
 }
 
+void GapBuf_delete(GapBuf* g, Index start, Index size) {
+}
 
-// todo: specifying new gap position
+// todo: specifying new gap position?
 void GapBuf_insert(GapBuf* g, Index start, Index size) {
-	if (g->gap >= size) {
-		// BBBBB........AAAA
-		//  ^6
-		// BnnnnnnBBBB..AAAA
-		if (start<=g->before) {
-			String_move(g->data, start+size, start, g->before-start);
-			g->before += size;
-		} else {
-			// before  gap
-			// BBBBB........AAAA
-			//               ^4
-			// BBBBB....AnnnnAAA
-			String_move(g->data, g->cont-size, g->cont, g->after-(g->data.length-start));
-			g->after += size;
-			g->cont -= size;
-		}
-		g->gap -= size;
+	if (g->gap < size) {
+		//error
+		return;
 	}
+	// BBBBB........AAAA
+	//  ^6
+	// BnnnnnnBBBB..AAAA
+	if (start<=g->before) {
+		String_move(g->data, start+size, start, g->before-start);
+		g->before += size;
+	} else {
+		// before  gap
+		// BBBBB........AAAA
+		//               ^4
+		// BBBBB....AnnnnAAA
+		String_move(g->data, g->cont-size, g->cont, g->after-(g->data.length-(start+g->gap)));
+		g->after += size;
+		g->cont -= size;
+	}
+	g->gap -= size;
 }
 
 void GapBuf_read(const GapBuf* g, Index start, const String s) {
@@ -77,6 +81,34 @@ void GapBuf_read(const GapBuf* g, Index start, const String s) {
 	}
 }
 
+void GapBuf_moveGap(GapBuf* g, Index gap) {
+	if (gap < g->before) { //gap is moving backwards
+		// BBBBBBBBBBB..AAAAAAAAAAA
+		//
+		// BBBBBBB..BBBBAAAAAAAAAAA
+		
+		// BBBBB........AAAA
+		//   ^     
+		// BB........BBBAAAA
+		Index amount = g->before - gap;
+		String_move(g->data, g->cont-amount, gap, amount);
+		g->before -= amount;
+		g->after += amount;
+		g->cont -= amount;
+	} else if (gap > g->data.length) {
+		// error
+	} else if (gap > g->before) { //moving forwards
+		// BBBBB........AAAA
+		//        ^         
+		// BBBBBAA........AA
+		Index amount = gap - g->before;
+		String_move(g->data, g->gap, g->cont, amount);
+		g->before += amount;
+		g->after -= amount;
+		g->cont += amount;
+	}
+}
+
 void GapBuf_write(const GapBuf* g, Index start, const String s) {
 	Index size = s.length;
 	if (start+size > g->data.length)
@@ -90,6 +122,14 @@ void GapBuf_write(const GapBuf* g, Index start, const String s) {
 		String_rtrans(s, 0, g->data, start, before);
 		String_rtrans(s, before, g->data, g->cont, size-before);
 	}
+}
+
+void GapBuf_debug(const GapBuf* g) {
+	fwrite(g->data.text, 1, g->before, stdout);
+	printf("|<-%ld->|", g->gap);
+	fwrite(&g->data.text[g->cont], 1, g->after, stdout);
+	//printf(" a:%ld", g->after);
+	fputc('\n', stdout);
 }
 
 void String_print(const String str) {
